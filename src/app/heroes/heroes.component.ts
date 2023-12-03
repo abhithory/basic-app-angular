dd916b39267a2b76e578c6c9454cceb2bae3e28f
abhithory <abhithory@gmail.com>
2023-12-03 06:01:10 -0600

    derrivatives used

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
+  heros: Hero[] = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
+  selectedHero?: Hero;
+
+  onSelect(here: Hero): void {
+    this.selectedHero = here;
+  };
 }
