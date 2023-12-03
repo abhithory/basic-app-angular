37973d22084b21f1e9b26c1b9dcc3f3b60acc570
abhithory <abhithory@gmail.com>
2023-12-03 06:07:43 -0600

    hero details component added

---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-hero-details',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './hero-details.component.html',
+  styleUrl: './hero-details.component.scss'
+})
+export class HeroDetailsComponent {
+
+  @Input() hero?: Hero;
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,11 +3,12 @@ import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailsComponent } from '../hero-details/hero-details.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, HeroDetailsComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
